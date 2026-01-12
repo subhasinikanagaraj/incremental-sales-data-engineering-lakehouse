1ff532b3d60c2f96176363f1327886414501166e
subhasinikanagaraj <subhasinikanagaraj91@gmail.com>
2026-01-12 11:54:04 -0600

    Create query.sql

---
+++ AzureSQLDB/query.sql
@@ -0,0 +1,15 @@
+CREATE TABLE src_car_sales
+(
+    Branch_ID Varchar(200),
+    Dealer_ID Varchar(200),
+    Model_ID Varchar(200),
+    Revenue BIGINT,
+    Units_Sold BIGINT,
+    Date_ID Varchar(200),
+    Day INT,
+    Month INT,
+    Year INT,
+    BranchName Varchar(2000),
+    DealerName Varchar(2000),
+    Product_Name Varchar(2000)
+);
